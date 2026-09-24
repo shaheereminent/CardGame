d33daa0ec8e0f4e22f4d37941abd641f7a1b1eaa
shaheereminent <shaheer_eminent@hotmail.com>
2026-09-25 04:28:16 +0500

    feat: add ability to reset game

--- script.js
+++ script.js
@@ -22,8 +22,10 @@ const CONFIG =
     GAMESTATE   : true,
     CURRENTSCORE: 0,
     ACTIVEPLAYER: 0,
-    MAX         : 100
+    MAX         : 20
 };
+
+console.log(`Current MAX SCORE: ${CONFIG.MAX}`);
 
 
 // Game States
@@ -50,6 +52,13 @@ const rollDice = function(diceNumber)
 };
 
 
+const addScoreToCurrentPlayer = function()
+    {
+        CONFIG.SCORES[CONFIG.ACTIVEPLAYER] += CONFIG.CURRENTSCORE;
+        DOM[`score${CONFIG.ACTIVEPLAYER}`].textContent = CONFIG.SCORES[CONFIG.ACTIVEPLAYER];
+    };
+
+
 const switchPlayer = function()
 {
     DOM[`current${CONFIG.ACTIVEPLAYER}`].textContent = 0;
@@ -57,6 +66,39 @@ const switchPlayer = function()
     CONFIG.ACTIVEPLAYER = CONFIG.ACTIVEPLAYER === 0 ? 1 : 0;
     DOM.player1.classList.toggle('player--active')
     DOM.player2.classList.toggle('player--active')
+};
+
+
+const checkWinner = function()
+{
+    if (CONFIG.SCORES[CONFIG.ACTIVEPLAYER] >= CONFIG.MAX)
+    {
+        CONFIG.GAMESTATE = false;
+        DOM[`player${CONFIG.ACTIVEPLAYER + 1}`].classList.add('player--winner');
+        DOM[`player${CONFIG.ACTIVEPLAYER + 1}`].classList.remove('player--active');
+        DOM.dice.classList.add('hidden');
+        return;
+    }
+    else
+    {
+        switchPlayer();
+    };
+};
+
+
+const resetGame = function()
+{
+    CONFIG.GAMESTATE = true;
+    CONFIG.SCORES = [0, 0];
+    DOM[`player${CONFIG.ACTIVEPLAYER + 1}`].classList.remove('player--active');
+    DOM[`player${CONFIG.ACTIVEPLAYER + 1}`].classList.remove('player--winner');
+    CONFIG.ACTIVEPLAYER = 0;
+    CONFIG.CURRENTSCORE = 0;
+    DOM.current0.textContent = 0;
+    DOM.current1.textContent = 0;
+    DOM.score0.textContent = 0;
+    DOM.score1.textContent = 0;
+    DOM.player1.classList.add('player--active');
 };
 
 
@@ -85,9 +127,9 @@ DOM.btnHold.addEventListener('click', function()
 {
     if (CONFIG.GAMESTATE)
     {
-        CONFIG.SCORES[CONFIG.ACTIVEPLAYER] += CONFIG.CURRENTSCORE
-        DOM[`score${CONFIG.ACTIVEPLAYER}`].textContent = CONFIG.SCORES[CONFIG.ACTIVEPLAYER]
-    
-        switchPlayer();
-    };
+        addScoreToCurrentPlayer();
+        checkWinner();
+    }
 });
+
+DOM.btnNew.addEventListener('click', () => resetGame());
